60e13879f1dc965fee2e01c27db33f961d00f957
Piotr Damer <piotrd@czuby.net>
2020-06-05 13:53:45 +0100

    Project renamed

--- poll/poll/asgi.py
+++ poll/poll/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for poll_app project.
+ASGI config for poll project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poll_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poll.settings')
 
 application = get_asgi_application()
