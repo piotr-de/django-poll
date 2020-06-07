80d9f6ecfda7a240386ae3ad1ccbb950be860cae
Piotr Damer <piotrd@czuby.net>
2020-06-07 14:20:10 +0100

    Initial index view logic

--- poll/poll_app/views.py
+++ poll/poll_app/views.py
@@ -2,14 +2,18 @@
 
 from django.http import HttpResponse
 
+from .models import Question
+
 def index(request):
-	return HttpResponse("Hello world")
+	latest = Question.objects.order_by('-pub_date')[:5]
+	output = '<br>'.join([f'{q.question_text} {q.pub_date}' for q in latest])
+	return HttpResponse(output)
 
 def detail(request, question_id):
-	return HttpResponse("Question %s:" % question_id)
+	return HttpResponse("Question %s: " % question_id)
 
 def results(request, question_id):
-	response = "Question %s results:"
+	response = "Question %s results: "
 	return HttpResponse(response % question_id)
 
 def vote(request, question_id):
