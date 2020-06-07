e81246bbd39f9ad9d4723e18d9433e06f918f656
Piotr Damer <piotrd@czuby.net>
2020-06-07 17:35:40 +0100

    Refactor the index view logic

--- poll/poll_app/views.py
+++ poll/poll_app/views.py
@@ -1,17 +1,14 @@
-# from django.shortcuts import render
+# from django.template import loader
 
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Question
 
 
 def index(request):
     latest = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('poll_app/index.html')
-    context = {
-        'latest': latest,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest': latest}
+    return render(request, 'poll_app/index.html', context)
 
 
 def detail(request, question_id):
