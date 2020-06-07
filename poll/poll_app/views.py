dd8ac7d2168dd142204cf2c55875ee06a01b6cdb
Piotr Damer <piotrd@czuby.net>
2020-06-07 12:58:24 +0100

    Create the core views

--- poll/poll_app/views.py
+++ poll/poll_app/views.py
@@ -1,5 +1,16 @@
 # from django.shortcuts import render
+
 from django.http import HttpResponse
 
 def index(request):
 	return HttpResponse("Hello world")
+
+def detail(request, question_id):
+	return HttpResponse("Question %s:" % question_id)
+
+def results(request, question_id):
+	response = "Question %s results:"
+	return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+	return HttpResponse(f"Vote on question {question_id}")		
