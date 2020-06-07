7731bfc7f686c8f2eaf94c4096324fc3d61dbc10
Piotr Damer <piotrd@czuby.net>
2020-06-07 16:57:16 +0100

    Index template setup

--- poll/poll_app/views.py
+++ poll/poll_app/views.py
@@ -1,20 +1,27 @@
 # from django.shortcuts import render
 
 from django.http import HttpResponse
-
+from django.template import loader
 from .models import Question
 
+
 def index(request):
-	latest = Question.objects.order_by('-pub_date')[:5]
-	output = '<br>'.join([f'{q.question_text} {q.pub_date}' for q in latest])
-	return HttpResponse(output)
+    latest = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('poll_app/index.html')
+    context = {
+        'latest': latest,
+    }
+    return HttpResponse(template.render(context, request))
+
 
 def detail(request, question_id):
-	return HttpResponse("Question %s: " % question_id)
+    return HttpResponse("Question %s: " % question_id)
+
 
 def results(request, question_id):
-	response = "Question %s results: "
-	return HttpResponse(response % question_id)
+    response = "Question %s results: "
+    return HttpResponse(response % question_id)
+
 
 def vote(request, question_id):
-	return HttpResponse(f"Vote on question {question_id}")		
+    return HttpResponse(f"Vote on question {question_id}")
