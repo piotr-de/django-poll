df056a858f83aecdc3f635a259354eec06c1b92e
Piotr Damer <piotrd@czuby.net>
2020-06-12 20:01:47 +0100

    Question.recent() test

--- poll/poll_app/tests.py
+++ poll/poll_app/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+
+class QuestionModelTests(TestCase):
+
+    def test_recent_with_future_pub_date(self):
+        future_time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=future_time)
+        self.assertFalse(future_question.recent())
