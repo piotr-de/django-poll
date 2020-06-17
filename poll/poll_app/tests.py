60bab17f66a8b86df714ae9b8caeb11bac5b0832
Piotr Damer <piotrd@czuby.net>
2020-06-17 16:53:51 +0100

    Added a further 2 tests

--- poll/poll_app/tests.py
+++ poll/poll_app/tests.py
@@ -11,4 +11,14 @@ class QuestionModelTests(TestCase):
     def test_recent_with_future_pub_date(self):
         future_time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=future_time)
-        self.assertFalse(future_question.recent())
+        self.assertIs(future_question.recent(), False)
+
+    def test_recent_with_old_question(self):
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.recent(), False)
+
+    def test_recent_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.recent(), True)
