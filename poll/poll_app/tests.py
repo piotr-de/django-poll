9b927ece6c56abb689a4e7f06e2b148c8a60f120
Piotr Damer <piotrd@czuby.net>
2020-06-21 12:52:44 +0100

    IndexView tests

--- poll/poll_app/tests.py
+++ poll/poll_app/tests.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
+from django.urls import reverse
 
 from .models import Question
 
@@ -19,6 +20,19 @@ class QuestionModelTests(TestCase):
         self.assertIs(old_question.recent(), False)
 
     def test_recent_with_recent_question(self):
-        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.recent(), True)
+
+
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_question(self):
+        response = self.client.get(reverse("poll_app:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls available.")
+        self.assertQuerysetEqual(response.context["latest"], [])
--- poll/poll_app/views.py
+++ poll/poll_app/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -14,7 +15,9 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last 5 questions"""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
