7c8d8f8574c7ae4bed571bb0f5b713406810b01d
Piotr Damer <piotrd@czuby.net>
2020-06-29 19:03:52 +0100

    Customize AdminView (2)

--- poll/poll_app/admin.py
+++ poll/poll_app/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
+
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
-        ("Date Information", {"fields": ["pub_date"]}),
+        ("Date Information", {
+            "fields": ["pub_date"], "classes": ["collapse"]
+        }),
     ]
+    inlines = [ChoiceInLine]
 
 
 admin.site.register(Question, QuestionAdmin)
